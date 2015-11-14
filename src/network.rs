2863e2233cdd5d10fd571b6ffb3472ecd9ed45c2
Boris Pfahringer <bpnzltd@gmail.com>
2015-11-14 22:54:53 +0100

    Add first bit of subnet function

--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,6 @@
 use petgraph;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 pub enum EdgeType {
     Pos,
@@ -9,3 +11,18 @@ pub type EdgeIndex = petgraph::graph::EdgeIndex;
 pub type NodeIndex = petgraph::graph::NodeIndex;
 
 pub type Network = petgraph::Graph<String, EdgeType>;
+
+pub trait SubNetwork {
+    fn subnet<N: AsRef<[NodeIndex]>>(&self, ns: N) -> Network;
+}
+
+impl SubNetwork for Network {
+    fn subnet<N: AsRef<[NodeIndex]>>(&self, ns: N) -> Network {
+        let ns = ns.as_ref();
+        let n_set: HashSet<NodeIndex> = HashSet::from_iter(ns.iter().cloned());
+        let subnet = Network::with_capacity(ns.len(), ns.len() * 2);
+        // IMPLEMENTATION
+
+        subnet
+    }
+}
