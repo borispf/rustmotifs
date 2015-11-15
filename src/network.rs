da5f769dbae865537e2c29be39333b59dd48ca32
Boris Pfahringer <bpnzltd@gmail.com>
2015-11-15 16:36:46 +0100

    Add basic test for subnet

--- src/network.rs
+++ src/network.rs
@@ -1,7 +1,7 @@
 use petgraph;
 use std::collections::HashMap;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub enum EdgeType {
     Pos,
     Neg,
@@ -33,4 +33,45 @@ impl SubNetwork for Network {
         }
         subnet
     }
+}
+
+#[test]
+fn test_network() {
+    use self::EdgeType::*;
+    let mut net = Network::new();
+    let a = net.add_node("a".to_string());
+    let b = net.add_node("b".to_string());
+    let c = net.add_node("c".to_string());
+    let d = net.add_node("d".to_string());
+    let e = net.add_node("e".to_string());
+
+    for u in &[a, b, c, d, e] {
+        net.add_edge(*u, *u, Pos);
+        for v in &[a, b, c, d, e] {
+            if u.index() < v.index() {
+                net.add_edge(*u, *v, Pos);
+                net.add_edge(*v, *u, Neg);
+            }
+        }
+    }
+    let sub = net.subnet(&[]);
+    assert_eq!(0, sub.node_count());
+    assert_eq!(0, sub.edge_count());
+
+    let sub = net.subnet(&[b]);
+    assert_eq!(1, sub.node_count());
+    assert_eq!(1, sub.edge_count());
+
+    let sub = net.subnet(&[a, c, e]);
+    assert_eq!(3, sub.node_count());
+    assert_eq!(9, sub.edge_count());
+
+    let sub = net.subnet(&[a, b, c, e]);
+    assert_eq!(4, sub.node_count());
+    assert_eq!(16, sub.edge_count());
+
+    let sub = net.subnet(&[e, d, c, b, a]);
+    assert_eq!(5, sub.node_count());
+    assert_eq!(25, sub.edge_count());
+
 }
