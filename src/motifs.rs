78b417f987b98018219b91032a2dc8feeecc278f
Boris Pfahringer <bpnzltd@gmail.com>
2015-11-15 22:14:09 +0100

    Fix small bug in all_motifs

--- src/motifs.rs
+++ src/motifs.rs
@@ -35,7 +35,7 @@ fn search_subset(k: usize,
     } else {
         hash.insert(s.clone());
         for u in s.clone() {
-            for v in net.neighbors(u) {
+            for v in net.neighbors_undirected(u) {
                 if !s.contains(&v) {
                     s.insert(v);
                     if !hash.contains(s) {
@@ -49,28 +49,63 @@ fn search_subset(k: usize,
 }
 
 #[test]
-fn test_motifs() {
+fn test_motifs_3() {
     let net = network_from_paper();
     let motifs = all_motifs(3, &net);
 
-    let feedforward = canonicalize(net.subnet(&[NodeIndex::new(2),
-                                                NodeIndex::new(11),
-                                                NodeIndex::new(12)]))
-                          .adjacency_matrix();
+    let feedforward = canonical_subnet(&net, &[3, 12, 13]).adjacency_matrix();
     assert_eq!(Some(&5), motifs.get(&feedforward));
 
-    let line = canonicalize(net.subnet(&[NodeIndex::new(0),
-                                         NodeIndex::new(1),
-                                         NodeIndex::new(15)]))
-                   .adjacency_matrix();
+    let line = canonical_subnet(&net, &[1, 2, 16]).adjacency_matrix();
     assert_eq!(Some(&10), motifs.get(&line));
 
-    let twofan = canonicalize(net.subnet(&[NodeIndex::new(4),
-                                           NodeIndex::new(9),
-                                           NodeIndex::new(12)]))
-                     .adjacency_matrix();
+    let twofan = canonical_subnet(&net, &[5, 10, 13]).adjacency_matrix();
     assert_eq!(Some(&3), motifs.get(&twofan));
 
+    let vee = canonical_subnet(&net, &[6, 9, 10]).adjacency_matrix();
+    assert_eq!(Some(&3), motifs.get(&vee));
 
-    assert_eq!(3, motifs.len());
+    assert_eq!(4, motifs.len());
+}
+
+#[test]
+fn test_motifs_4() {
+    let net = network_from_paper();
+    let motifs = all_motifs(4, &net);
+
+    let line = canonical_subnet(&net, &[1, 2, 15, 16]).adjacency_matrix();
+    assert_eq!(Some(&5), motifs.get(&line));
+
+    let feedforwardin = canonical_subnet(&net, &[1, 14, 15, 16]).adjacency_matrix();
+    assert_eq!(Some(&2), motifs.get(&feedforwardin));
+
+    let feedforwardout = canonical_subnet(&net, &[1, 2, 8, 16]).adjacency_matrix();
+    assert_eq!(Some(&2), motifs.get(&feedforwardout));
+
+    let feedforwardsidein = canonical_subnet(&net, &[3, 5, 12, 13]).adjacency_matrix();
+    assert_eq!(Some(&3), motifs.get(&feedforwardsidein));
+
+    let feedforwardsideout = canonical_subnet(&net, &[5, 6, 9, 10]).adjacency_matrix();
+    assert_eq!(Some(&1), motifs.get(&feedforwardsideout));
+
+    let branch = canonical_subnet(&net, &[5, 6, 9, 13]).adjacency_matrix();
+    assert_eq!(Some(&5), motifs.get(&branch));
+
+    let n = canonical_subnet(&net, &[13, 10, 3, 5]).adjacency_matrix();
+    assert_eq!(Some(&4), motifs.get(&n));
+
+    let feedforwardendin = canonical_subnet(&net, &[4, 5, 6, 10]).adjacency_matrix();
+    assert_eq!(Some(&1), motifs.get(&feedforwardendin));
+
+    let branch_feedforward = canonical_subnet(&net, &[5, 6, 10, 13]).adjacency_matrix();
+    assert_eq!(Some(&1), motifs.get(&branch_feedforward));
+
+    println!("{:?}", motifs);
+
+    assert_eq!(9, motifs.len());
+}
+
+#[cfg(test)]
+fn canonical_subnet(net: &Network, ns: &[usize]) -> Network {
+    canonicalize(net.subnet(&Vec::from_iter(ns.iter().map(|n| NodeIndex::new(*n - 1)))))
 }
